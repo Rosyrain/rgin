8a09df281704c7385462e0544d4e7b7376eef141
Rosyrain <116946548+Rosyrain@users.noreply.github.com>
2025-02-24 12:28:53 +0800

    Modify information to implement go install

--- internal/template/template.go
+++ internal/template/template.go
@@ -1,14 +1,16 @@
 package template
 
 import (
-	"os"
+	"embed"
 	"path/filepath"
 	"text/template"
 )
 
+var templateFS embed.FS
+
 func LoadTemplate(name string) (*template.Template, error) {
 	tmplPath := filepath.Join("template", name)
-	data, err := os.ReadFile(tmplPath)
+	data, err := templateFS.ReadFile(tmplPath)
 	if err != nil {
 		return nil, err
 	}
