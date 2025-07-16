06586e81bef3d383bf7a6ddceb43bb0fea5fc354
rosyrain <jx178861659@163.com>
2025-07-16 12:56:25 +0800

    Remove unnecessary information

--- internal/template/template.go
+++ internal/template/template.go
@@ -10,8 +10,7 @@ import (
 //go:embed templates/*
 var templateFS embed.FS
 
-//go:embed templates/example/*
-var ExampleFS embed.FS
+// 已去除对 templates/example/* 的 embed，避免跨 module 报错
 
 func init() {
 	// 遍历检查嵌入的模板
@@ -19,29 +18,17 @@ func init() {
 		if err != nil {
 			return err
 		}
-		fmt.Println("Embedded file:", path)
+		// fmt.Println("Embedded file:", path)
 		return nil
 	})
 	if err != nil {
 		fmt.Println("embed files failed,err: ", err)
 	}
-
-	// 遍历检查嵌入的示例代码
-	err = fs.WalkDir(ExampleFS, ".", func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		fmt.Println("Embedded example file:", path)
-		return nil
-	})
-	if err != nil {
-		fmt.Println("embed example files failed,err: ", err)
-	}
 }
 
 func LoadTemplate(name string) (*template.Template, error) {
 	tmplPath := "templates" + name
-	fmt.Println("path: ", tmplPath)
+	// fmt.Println("path: ", tmplPath)
 	data, err := templateFS.ReadFile(tmplPath)
 	if err != nil {
 		return nil, err
